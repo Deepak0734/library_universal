7fa50b97e47f274800cb9d94864f9f534edf0078
Deepak0734 <dr941954@gmail.com>
2024-09-07 19:07:02 +0530

    Update bookstorage.js
    js script change

--- bookstorage.js
+++ bookstorage.js
@@ -1,14 +1,91 @@
 const books = {
     1: [
         { subject: 'Math', bookLink: 'link-to-class1-math-book', solutionLink: 'link-to-class1-math-solution' },
-        { subject: 'Science', bookLink: 'link-to-class1-science-book', solutionLink: 'link-to-class1-science-solution' }
+        { subject: 'Science', bookLink: 'link-to-class1-science-book', solutionLink: 'link-to-class1-science-solution' },
+        { subject: 'english', bookLink: 'link-to-class1-science-book', solutionLink: 'link-to-class1-science-solution' },
+        { subject: 'Hindi', bookLink: 'link-to-class1-science-book', solutionLink: 'link-to-class1-science-solution' },
+        { subject: 'social-science', bookLink: 'link-to-class1-science-book', solutionLink: 'link-to-class1-science-solution' }
+        
     ],
     2: [
         { subject: 'Math', bookLink: 'link-to-class2-math-book', solutionLink: 'link-to-class2-math-solution' },
-        { subject: 'Science', bookLink: 'link-to-class2-science-book', solutionLink: 'link-to-class2-science-solution' }
+        { subject: 'Science', bookLink: 'link-to-class2-science-book', solutionLink: 'link-to-class2-science-solution' },
+        { subject: 'english', bookLink: 'link-to-class1-science-book', solutionLink: 'link-to-class1-science-solution' },
+        { subject: 'Hindi', bookLink: 'link-to-class1-science-book', solutionLink: 'link-to-class1-science-solution' },
+        { subject: 'social-science', bookLink: 'link-to-class1-science-book', solutionLink: 'link-to-class1-science-solution' }
+    ],
+    3: [
+        { subject: 'Math', bookLink: 'link-to-class2-math-book', solutionLink: 'link-to-class2-math-solution' },
+        { subject: 'Science', bookLink: 'link-to-class2-science-book', solutionLink: 'link-to-class2-science-solution' },
+        { subject: 'english', bookLink: 'link-to-class1-science-book', solutionLink: 'link-to-class1-science-solution' },
+        { subject: 'Hindi', bookLink: 'link-to-class1-science-book', solutionLink: 'link-to-class1-science-solution' },
+        { subject: 'social-science', bookLink: 'link-to-class1-science-book', solutionLink: 'link-to-class1-science-solution' }
+    ],
+    4: [
+        { subject: 'Math', bookLink: 'link-to-class2-math-book', solutionLink: 'link-to-class2-math-solution' },
+        { subject: 'Science', bookLink: 'link-to-class2-science-book', solutionLink: 'link-to-class2-science-solution' },
+        { subject: 'english', bookLink: 'link-to-class1-science-book', solutionLink: 'link-to-class1-science-solution' },
+        { subject: 'Hindi', bookLink: 'link-to-class1-science-book', solutionLink: 'link-to-class1-science-solution' },
+        { subject: 'social-science', bookLink: 'link-to-class1-science-book', solutionLink: 'link-to-class1-science-solution' }
+    ],
+    5: [
+        { subject: 'Math', bookLink: 'link-to-class2-math-book', solutionLink: 'link-to-class2-math-solution' },
+        { subject: 'Science', bookLink: 'link-to-class2-science-book', solutionLink: 'link-to-class2-science-solution' },
+        { subject: 'english', bookLink: 'link-to-class1-science-book', solutionLink: 'link-to-class1-science-solution' },
+        { subject: 'Hindi', bookLink: 'link-to-class1-science-book', solutionLink: 'link-to-class1-science-solution' },
+        { subject: 'social-science', bookLink: 'link-to-class1-science-book', solutionLink: 'link-to-class1-science-solution' }
+    ],
+    6: [
+        { subject: 'Math', bookLink: 'link-to-class2-math-book', solutionLink: 'link-to-class2-math-solution' },
+        { subject: 'Science', bookLink: 'link-to-class2-science-book', solutionLink: 'link-to-class2-science-solution' },
+        { subject: 'english', bookLink: 'link-to-class1-science-book', solutionLink: 'link-to-class1-science-solution' },
+        { subject: 'Hindi', bookLink: 'link-to-class1-science-book', solutionLink: 'link-to-class1-science-solution' },
+        { subject: 'social-science', bookLink: 'link-to-class1-science-book', solutionLink: 'link-to-class1-science-solution' }
+    ],
+    7: [
+        { subject: 'Math', bookLink: 'link-to-class2-math-book', solutionLink: 'link-to-class2-math-solution' },
+        { subject: 'Science', bookLink: 'link-to-class2-science-book', solutionLink: 'link-to-class2-science-solution' },
+        { subject: 'english', bookLink: 'link-to-class1-science-book', solutionLink: 'link-to-class1-science-solution' },
+        { subject: 'Hindi', bookLink: 'link-to-class1-science-book', solutionLink: 'link-to-class1-science-solution' },
+        { subject: 'social-science', bookLink: 'link-to-class1-science-book', solutionLink: 'link-to-class1-science-solution' }
+    ],
+    8: [
+        { subject: 'Math', bookLink: 'link-to-class2-math-book', solutionLink: 'link-to-class2-math-solution' },
+        { subject: 'Science', bookLink: 'link-to-class2-science-book', solutionLink: 'link-to-class2-science-solution' },
+        { subject: 'english', bookLink: 'link-to-class1-science-book', solutionLink: 'link-to-class1-science-solution' },
+        { subject: 'Hindi', bookLink: 'link-to-class1-science-book', solutionLink: 'link-to-class1-science-solution' },
+        { subject: 'social-science', bookLink: 'link-to-class1-science-book', solutionLink: 'link-to-class1-science-solution' }
+    ],
+    9: [
+        { subject: 'Math', bookLink: 'link-to-class2-math-book', solutionLink: 'link-to-class2-math-solution' },
+        { subject: 'Science', bookLink: 'link-to-class2-science-book', solutionLink: 'link-to-class2-science-solution' },
+        { subject: 'english', bookLink: 'link-to-class1-science-book', solutionLink: 'link-to-class1-science-solution' },
+        { subject: 'Hindi', bookLink: 'link-to-class1-science-book', solutionLink: 'link-to-class1-science-solution' },
+        { subject: 'social-science', bookLink: 'link-to-class1-science-book', solutionLink: 'link-to-class1-science-solution' }
+    ],
+    10: [
+        { subject: 'Math', bookLink: 'link-to-class2-math-book', solutionLink: 'link-to-class2-math-solution' },
+        { subject: 'Science', bookLink: 'link-to-class2-science-book', solutionLink: 'link-to-class2-science-solution' },
+        { subject: 'english', bookLink: 'link-to-class1-science-book', solutionLink: 'link-to-class1-science-solution' },
+        { subject: 'Hindi', bookLink: 'link-to-class1-science-book', solutionLink: 'link-to-class1-science-solution' },
+        { subject: 'social-science', bookLink: 'link-to-class1-science-book', solutionLink: 'link-to-class1-science-solution' }
+    ],
+    11: [
+        { subject: 'Math', bookLink: 'link-to-class2-math-book', solutionLink: 'link-to-class2-math-solution' },
+        { subject: 'Science', bookLink: 'link-to-class2-science-book', solutionLink: 'link-to-class2-science-solution' },
+        { subject: 'english', bookLink: 'link-to-class1-science-book', solutionLink: 'link-to-class1-science-solution' },
+        { subject: 'Hindi', bookLink: 'link-to-class1-science-book', solutionLink: 'link-to-class1-science-solution' },
+        { subject: 'social-science', bookLink: 'link-to-class1-science-book', solutionLink: 'link-to-class1-science-solution' }
+    ],
+    12: [
+        { subject: 'Math', bookLink: 'link-to-class2-math-book', solutionLink: 'link-to-class2-math-solution' },
+        { subject: 'Science', bookLink: 'link-to-class2-science-book', solutionLink: 'link-to-class2-science-solution' },
+        { subject: 'english', bookLink: 'link-to-class1-science-book', solutionLink: 'link-to-class1-science-solution' },
+        { subject: 'Hindi', bookLink: 'link-to-class1-science-book', solutionLink: 'link-to-class1-science-solution' },
+        { subject: 'social-science', bookLink: 'link-to-class1-science-book', solutionLink: 'link-to-class1-science-solution' }
     ],
     // Continue for other classes...
-    12: [
+    Bsc: [
         { subject: 'Physics', bookLink: 'link-to-class12-physics-book', solutionLink: 'link-to-class12-physics-solution' },
         { subject: 'Chemistry', bookLink: 'link-to-class12-chemistry-book', solutionLink: 'link-to-class12-chemistry-solution' }
     ]
